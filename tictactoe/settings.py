cc5edf1f537a35f065d3a94954c4c259f8b0fe3c
Juan Lagunas <juanlagunas28@gmail.com>
2023-06-01 00:55:37 -0400

    Setup up for railway

--- tictactoe/settings.py
+++ tictactoe/settings.py
@@ -131,10 +131,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIR = [
-    os.path.join(BASE_DIR, 'static')
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
