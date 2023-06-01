29df9356f934d38f175a4c08df0b6110eadffa56
Juan Lagunas <juanlagunas28@gmail.com>
2023-05-31 22:56:28 -0400

    New tictactoe

--- tictactoe/settings.py
+++ tictactoe/settings.py
@@ -86,7 +86,13 @@ WSGI_APPLICATION = 'tictactoe.wsgi.app'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
